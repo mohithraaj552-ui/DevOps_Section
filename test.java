fc6fa5033b2b99ab0ffd13fae07ec2b2719413ce
mohithraaj552-ui <mohithraaj552@gmail.com>
2025-09-22 12:50:24 +0530

    Add files via upload
    Initial commit

---
+++ test.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class test {
+    public static void main(String[] args) {
+        // Create an instance of the Sample class
+        test obj = new test();
+        
+        // Call the greet method
+        obj.greet();
+    }
+
+    // Method to display a greeting message
+    public void greet() {
+        System.out.println("Hello, welcome to the Sample class!");
+    }
+}
+
